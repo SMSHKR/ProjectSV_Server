0a2f56f9e3a1a5fdb037688e8ec7437a19c92297
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2021-02-05 10:19:10 +0700

    startapp upload

--- projectsv_server/settings.py
+++ projectsv_server/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'rest_api.apps.RestApiConfig',
+    'upload.apps.UploadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
