88494887ecf4b85254bdb3c5909caf46b0187cbc
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2021-02-05 12:12:05 +0700

    Upload File to /media succeed

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+def upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        return HttpResponse(fs.url(filename))
+    return HttpResponse("Test Page")
