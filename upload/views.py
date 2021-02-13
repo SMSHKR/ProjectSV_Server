5bb4ea365745021365306d906dbefdeb8112c50e
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2021-02-13 15:55:32 +0700

    Receive train images, (static) JSON Response

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
+
+import uuid
 
 # Create your views here.
 def test(request):
@@ -12,4 +15,14 @@ def test(request):
     return HttpResponse("Test Page")
 
 def train(request):
-    return HttpResponse("Train Page")
+    if request.method == 'POST':
+        files = request.FILES.getlist('images')
+        fs = FileSystemStorage()
+        for file in files:
+            fs.save(file.name, file)
+        response = {
+            "test": 1,
+            "model": uuid.uuid4()
+        }
+        return JsonResponse(response, safe=False)
+    return HttpResponseNotFound()
